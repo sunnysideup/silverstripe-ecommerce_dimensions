c278157255464c1f33f8186afb5aade788c4ec2f
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-07-14 13:45:39 +0000

    some refinement to measurement module
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_dimensions/trunk@1323 950b781c-c544-efab-2f99-90083ee69683

--- code/model/EcommerceDimension.php
+++ code/model/EcommerceDimension.php
@@ -4,7 +4,7 @@ class EcommerceDimension extends DataObject {
 
 	public static $db = array(
 		"Measurement" => "Varchar(100)",
-		"Measure" => "Double",
+		"Measure" => "Double(4,9)",
 		"Unit" => "Varchar(100)"
 	);
 
@@ -20,7 +20,19 @@ class EcommerceDimension extends DataObject {
 
 	public static $default_sort = "";
 
+	public static $searchable_fields = array("Measure" => "PartialMatchFilter", "Unit" => "PartialMatchFilter", "Measure" );
 
+	public static $casting array(
+		"Title" => "Text"
+	) 
+
+	function Title() {
+		return $this->Measurement.": ".$this->Measure.$this->Unit;
+	}
+	
+	public static $field_labels = array("Measurement" => "Measurement (e.g. Width)", "Measure" => "Measure (e.g. 100)", "Unit" => "Unit (e.g. cm)");
+
+	public static $summary_fields = array("Measurement" => "Measurement", "Measure" => "Measure", "Unit" => "Unit");
 }
 
 
