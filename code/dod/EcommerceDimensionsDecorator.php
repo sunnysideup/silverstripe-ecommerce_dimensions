860a84726becdda69da53ee2294e4733ce6d2857
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-23 16:33:01 +1300

    PATCH: module clean-up

--- code/dod/EcommerceDimensionsDecorator.php
+++ code/dod/EcommerceDimensionsDecorator.php
@@ -1,32 +1,31 @@
 <?php
 
 
-class EcommerceDimensionsDecorator extends DataObjectDecorator {
+class EcommerceDimensionsDecorator extends DataObjectDecorator
+{
+    public function extraStatics()
+    {
+        return array(
+            "has_many" => array(
+                "EcommerceDimensions" => "EcommerceDimension"
+            )
+        );
+    }
 
-	function extraStatics () {
-		return array(
-			"has_many" => array(
-				"EcommerceDimensions" => "EcommerceDimension"
-			)
-		);
-	}
-
-	function updateCMSFields(&$fields) {
-		$fields->addFieldToTab(
-			"Root.Content.Dimensions",
-			new HasManyComplexTableField(
-				$controller = $this->owner,
-				$name = "EcommerceDimensions",
-				$sourceClass = "EcommerceDimension",
-				$fieldList = null,
-				$detailFormFields = null,
-				$sourceFilter = "ProductID = ".$this->owner->ID,
-				$sourceSort = "",
-				$sourceJoin = ""
-			)
-		);
-	}
-
-
-} 
- 
+    public function updateCMSFields(&$fields)
+    {
+        $fields->addFieldToTab(
+            "Root.Content.Dimensions",
+            new HasManyComplexTableField(
+                $controller = $this->owner,
+                $name = "EcommerceDimensions",
+                $sourceClass = "EcommerceDimension",
+                $fieldList = null,
+                $detailFormFields = null,
+                $sourceFilter = "ProductID = ".$this->owner->ID,
+                $sourceSort = "",
+                $sourceJoin = ""
+            )
+        );
+    }
+}
--- code/model/EcommerceDimension.php
+++ code/model/EcommerceDimension.php
@@ -1,40 +1,39 @@
 <?php
 
-class EcommerceDimension extends DataObject {
+class EcommerceDimension extends DataObject
+{
+    public static $db = array(
+        "Measurement" => "Varchar(100)",
+        "Measure" => "Double(4,9)",
+        "Unit" => "Varchar(100)"
+    );
 
-	public static $db = array(
-		"Measurement" => "Varchar(100)",
-		"Measure" => "Double(4,9)",
-		"Unit" => "Varchar(100)"
-	);
+    public static $has_one = array(
+        "Product" => "Product"
+    );
 
-	public static $has_one = array(
-		"Product" => "Product"
-	);
+    public static $singular_name = "Measurement";
 
-	public static $singular_name = "Measurement";
+    public static $plural_name = "Measurements";
 
-	public static $plural_name = "Measurements";
+    public static $default_sort = "";
 
-	public static $default_sort = "";
+    public static $searchable_fields = array("Measure" => "PartialMatchFilter", "Unit" => "PartialMatchFilter", "Measure");
 
-	public static $searchable_fields = array("Measure" => "PartialMatchFilter", "Unit" => "PartialMatchFilter", "Measure");
+    public static $casting = array(
+        "Title" => "Text"
+    );
 
-	public static $casting = array(
-		"Title" => "Text"
-	);
+    public function Title()
+    {
+        return $this->getTitle();
+    }
+    public function getTitle()
+    {
+        return $this->Measurement.": ".$this->Measure.$this->Unit;
+    }
 
-	function Title() {return $this->getTitle();}
-	function getTitle() {
-		return $this->Measurement.": ".$this->Measure.$this->Unit;
-	}
+    public static $field_labels = array("Measurement" => "Measurement (e.g. Width)", "Measure" => "Measure (e.g. 100)", "Unit" => "Unit (e.g. cm)");
 
-	public static $field_labels = array("Measurement" => "Measurement (e.g. Width)", "Measure" => "Measure (e.g. 100)", "Unit" => "Unit (e.g. cm)");
-
-	public static $summary_fields = array("Measurement" => "Measurement", "Measure" => "Measure", "Unit" => "Unit");
+    public static $summary_fields = array("Measurement" => "Measurement", "Measure" => "Measure", "Unit" => "Unit");
 }
-
-
-
-
-
